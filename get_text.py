cdaf3680a4d49f1a0fff38f806eb5781ca0c1635
Jacob Lebowitz <31675118+AugmentedMode@users.noreply.github.com>
2019-06-20 18:43:35 -0500

    updating get_text.py

--- get_text.py
+++ get_text.py
@@ -1,20 +1,26 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
 import PyPDF2
+import io
+import requests
 
-file_name = 'example.pdf'
-all_words = [] # List to hold strings from all pages in a PDF
+url = 'https://literature.rockwellautomation.com/idc/groups/literature/documents/rn/1441-rn002_-en-p.pdf'
+all_content = [] # List to hold strings from all pages in a PDF
 
-def get_text():
 
-    pdf_file = open(file_name, 'rb') # Opens PDF file
-    read_pdf = PyPDF2.PdfFileReader(pdf_file) # Reads in PDF
+def get_text(url):
+
+    r = requests.get(url) # Grab URL
+    pdf_file = io.BytesIO(r.content) # Downloads PDF file as a byte stream (only in memory never saved locally)
+    #pdf_file = open(file_name, 'rb') # Opens PDF file from directory
+    read_pdf = PdfFileReader(pdf_file) # Reads in PDF
     number_of_pages = read_pdf.getNumPages() # Gets number of pages
 
     for pages in range(number_of_pages): # loops through all pages in PDF
         page = read_pdf.getPage(pages)
-        page_content = page.extractText() # Extracts text from current page
-        all_words.append(page_content.encode('utf-8')) # Adds string of text from current page to list
+        page_content = page.extractText().replace('\n', '') # Extracts text from current page & cleans up newlines
+        all_content.append(page_content.encode('utf-8')) # Adds string of text from current page to list
 
-    return number_of_pages, all_words
+    return number_of_pages, all_content
 
 
-number_of_pages, all_words = get_text()
+number_of_pages, all_words = get_text(url)
