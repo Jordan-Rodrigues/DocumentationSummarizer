40a328b5510d3daf0704088850ec8fc9ac21bc70
Jordan Rodrigues <JRodri85@rockwellautomation.com>
2019-06-25 14:33:01 -0500

    Add speech-to-text functionality

--- WebAppCode/static/scripts/chatbot.js
+++ WebAppCode/static/scripts/chatbot.js
@@ -4,18 +4,22 @@ $(document).ready(function () {
 
 $("#inputMessage").keypress(function (event) {
   if (event.which == 13) {
-    userSend()
+    var truth = userSend()
     chatScroll()
+    if (truth) {
     setTimeout(robotSend, 2000)
     setTimeout(chatScroll, 2000)
+    }
   }
 });
 
 $(".sendButton").click(function () {
-  userSend()
+  var truth = userSend()
   chatScroll()
+  if (truth) {
   setTimeout(robotSend, 2000)
   setTimeout(chatScroll, 2000)
+  }
 })
 
 $("#target").submit(function (e) {
@@ -26,14 +30,18 @@ $("#target").submit(function (e) {
 
 function userSend() {
   if ($(".input").val() != "") {
-      $(' <div class="userSection"> ' +
+    $(' <div class="userSection"> ' +
       '<div class="userBubble">' +
       '<h5 class="userMessage">' + $(".input").val() + '</h5>' +
       '</div>' +
       '<img class="userIcon" src="static/css/Images/person.png">' +
       '<hr class="chatBreaker">' +
       '</div>').hide().appendTo(".chatMed").fadeIn(1500)
-    $(".input").val('')
+      $(".input").val('')
+      return true;
+  }
+  else {
+    return false;
   }
 }
 
@@ -43,9 +51,57 @@ function chatScroll() {
 
 function robotSend() {
   $(' <div class="botSection"> ' + '<img class="botIcon" src="static/css/Images/robot4.png">' +
-  '<div class="botBubble">' +
-  '<h5 class="botMessage">' + "Sorry I can't help you with that at this moment." + '</h5>' +
-  '</div>' +
-  '<hr class="chatBreaker">' +
-  '</div>').hide().appendTo(".chatMed").fadeIn(1500)
+    '<div class="botBubble">' +
+    '<h5 class="botMessage">' + "Sorry I can't help you with that at this moment." + '</h5>' +
+    '</div>' +
+    '<hr class="chatBreaker">' +
+    '</div>').hide().appendTo(".chatMed").fadeIn(1500)
+    $(".input").val('')
 }
+
+
+//This section controls the Mic option. IMPORTANT: If you don't speak into the mic after calling, it breaks everything.
+function textSpeech() {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  final_transcript = '';
+  recognition.start();
+  $("#inputMessage").val("   Feel free to speak. ROKBot is listening!")
+}
+
+var final_transcript = '';
+var recognizing = false;
+var recognition = new webkitSpeechRecognition();
+recognition.onstart = function () {
+  recognizing = true;
+};
+
+recognition.onresult = function (event) {
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      final_transcript += event.results[i][0].transcript;
+      final_transcript = capitalize(final_transcript);
+      $("#inputMessage").val(final_transcript)
+      final_transcript = ''
+      $('.sendButton').trigger('click');
+      recognizing = false;
+      $("#inputMessage").val("")
+    };
+
+    /** Functions to do line breaks */
+    var two_line = /\n\n/g;
+    var one_line = /\n/g;
+
+    function linebreak(s) {
+      return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+    }
+    var first_char = /\S/;
+
+    function capitalize(s) {
+      return s.replace(first_char, function (m) {
+        return m.toUpperCase();
+      });
+    }
+
+  }
--- WebAppCode/templates/static/scripts/chatbot.js
+++ WebAppCode/templates/static/scripts/chatbot.js
@@ -26,7 +26,7 @@ $("#target").submit(function (e) {
 
 function userSend() {
   if ($(".input").val() != "") {
-      $(' <div class="userSection"> ' +
+    $(' <div class="userSection"> ' +
       '<div class="userBubble">' +
       '<h5 class="userMessage">' + $(".input").val() + '</h5>' +
       '</div>' +
@@ -43,9 +43,61 @@ function chatScroll() {
 
 function robotSend() {
   $(' <div class="botSection"> ' + '<img class="botIcon" src="static/css/Images/robot4.png">' +
-  '<div class="botBubble">' +
-  '<h5 class="botMessage">' + "Sorry I can't help you with that at this moment." + '</h5>' +
-  '</div>' +
-  '<hr class="chatBreaker">' +
-  '</div>').hide().appendTo(".chatMed").fadeIn(1500)
+    '<div class="botBubble">' +
+    '<h5 class="botMessage">' + "Sorry I can't help you with that at this moment." + '</h5>' +
+    '</div>' +
+    '<hr class="chatBreaker">' +
+    '</div>').hide().appendTo(".chatMed").fadeIn(1500)
+}
+
+function textSpeech() {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.start();
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    }
+
+var final_transcript = '';
+var recognizing = false;
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.onstart = function() {
+recognizing = true;
+};
+recognition.onend = function() {
+  recognizing = false;
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+    var range = document.createRange();
+    range.selectNode(document.getElementById('final_span'));
+    window.getSelection().addRange(range);
+  }
+};
+recognition.onresult = function(event) {
+  var interim_transcript = '';
+  for (var i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      final_transcript += event.results[i][0].transcript;
+    } else {
+      interim_transcript += event.results[i][0].transcript;
+    }
+  }
+  final_transcript = capitalize(final_transcript);
+  final_span.innerHTML = linebreak(final_transcript);
+  interim_span.innerHTML = linebreak(interim_transcript);
+};
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+var first_char = /\S/;
+function capitalize(s) {
+return s.replace(first_char, function(m) { return m.toUpperCase(); });
 }
