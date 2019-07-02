b5a937c1b0809df2162543428dabf15c44d643aa
pmtell123 <51960390+pmtell123@users.noreply.github.com>
2019-07-02 11:02:06 -0500

    pdf_parser.py update
    Worked more functions that will take out all  of the stop words that are not needed so we can focus in on the more important ones.

--- pdf_parser.py
+++ pdf_parser.py
@@ -15,7 +15,7 @@ all_urls = ['https://literature.rockwellautomation.com/idc/groups/literature/doc
 url = 'https://literature.rockwellautomation.com/idc/groups/literature/documents/rn/1441-rn002_-en-p.pdf'
 all_content = [] # List to hold strings from all pages in a PDF
 
-
+##################FUNCTIONS####################################################
 def get_text(url):
 
     r = requests.get(url) # Grab URL
@@ -45,22 +45,40 @@ def size_of(words):
     for i in range(0, len(split_words)):
         count += 1
     return count
-number_of_pages, all_words = get_text(url)
 
+#A helper function that will be called upon when we want to remove specific stop words
+#Parameters: Takes in list_words to take out all of the stop words needed
+def word_stop(list_words):
+    stop_words = set(stopwords.words("english"))
+    words = word_tokenize(str(list_words))
+    filtered_sentence = []
+    removed_words = []
+    for w in words:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+        else:
+            removed_words.append(w)
+    return filtered_sentence
+
+#This function will take in words and print them to the console for testing purposes
+#mostly
+def to_string(printed_words):
+    for i in range(0, len(printed_words)):
+        print(printed_words[i]),
+
+
+###############DRIVER###################
+number_of_pages, all_words = get_text(url)
 #Use a new list to hold all of the split words so we can later count how many words are actually there.
 split_words = []
 for i in range(0, len(all_words)):
     split_words += convert_to_string(all_words[i])
 
-stop_words = set(stopwords.words("english"))
-words = word_tokenize(str(all_words[0]))
-filtered_sentence = []
-for w in words:
-    if w not in stop_words:
-        filtered_sentence.append(w)
+stopped_words = []
+for i in range(0, len(all_words)):
+    stopped_words.append(word_stop(all_words[i]))
 
-print(filtered_sentence)
-print(all_words[0])
+to_string(stopped_words)
 #print(all_words)
 #prints the count at the end of iteration
 #print(size_of(split_words))
